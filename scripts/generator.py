266c43731ab1b5ffe9ccbc5afdcbf263ac8efd53
wetdreams69 <145813305+wetdreams69@users.noreply.github.com>
2023-10-17 10:36:23 -0300

    Update generator.py

--- scripts/generator.py
+++ scripts/generator.py
@@ -5,14 +5,19 @@ import os
 import sys
 import streamlink
 import logging
-#import json
+import json
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)  
 
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)  
-logger.addHandler(console_handler)
+log_file = "output.log" 
+file_handler = RotatingFileHandler(log_file, maxBytes=100000, backupCount=1)
+file_handler.setLevel(logging.DEBUG) 
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 banner = r'''
 ######################################################################
@@ -114,9 +119,7 @@ with open("playlist.m3u", "w") as f:
             f.write(item['url'])
             f.write('\n')
 
-#with open("playlist.json", "w") as f:
-
-#json_data = json.dumps(channel_data, indent=2)
-
-#print(json_data)
+with open("playlist.json", "w") as f:
+    json_data = json.dumps(channel_data, indent=2)
+    f.write(json_data)
 
