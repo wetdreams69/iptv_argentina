416de7a1283b3319cdcd7ba2b207969d77818e89
wetdreams69 <145813305+wetdreams69@users.noreply.github.com>
2023-10-17 09:11:34 -0300

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -4,8 +4,6 @@ echo $(dirname $0)
 
 python3 -m pip install requests streamlink
 
-cd $(dirname $0)/scripts/
-
-python3 generator.py > ../playlist.m3u
+python3 $(dirname $0)/scripts/generator.py
 
 echo Done!
